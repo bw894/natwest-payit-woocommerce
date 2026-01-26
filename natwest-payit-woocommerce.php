9a121244a6c2bd08b8bc46639ee396f0b1dab756
bw894 <bw894@bath.ac.uk>
2026-01-26 18:03:05 +0000

    Update natwest-payit-woocommerce.php

--- natwest-payit-woocommerce.php
+++ natwest-payit-woocommerce.php
@@ -15,3 +15,9 @@ add_action('plugins_loaded', function () {
     require_once NATWEST_PAYIT_PLUGIN_PATH . 'includes/class-natwest-payit-loader.php';
     NatWest_PayIt_Loader::init();
 });
+
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
+    $settings_url = admin_url('admin.php?page=natwest-payit');
+    array_unshift($links, '<a href="' . esc_url($settings_url) . '">Settings</a>');
+    return $links;
+});
