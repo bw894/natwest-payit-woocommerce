9717113a22d4db8a8b02c1e117ad0d36e79211fd
bw894 <bw894@bath.ac.uk>
2026-01-26 17:38:13 +0000

    Update class-natwest-payit-logger.php

--- includes/helpers/class-natwest-payit-logger.php
+++ includes/helpers/class-natwest-payit-logger.php
@@ -3,6 +3,12 @@ if (!defined('ABSPATH')) exit;
 
 class NatWest_PayIt_Logger {
 
+    $options = NatWest_PayIt_Admin::get();
+
+    if (empty($options['logging'])) {
+        return;
+}
+
     public static function log($message, $level = 'info') {
         if (!class_exists('WC_Logger')) return;
 
