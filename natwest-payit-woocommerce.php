b652eb7d7496f5cae21f3b3baae738b9e6a2fbf8
bw894 <bw894@bath.ac.uk>
2026-01-26 18:00:05 +0000

    Update natwest-payit-woocommerce.php

--- natwest-payit-woocommerce.php
+++ natwest-payit-woocommerce.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: NatWest PayIt for WooCommerce
  * Description: NatWest PayIt (Pay by Bank) Hosted Journey integration for WooCommerce
- * Version: 1.0.0
+ * Version: 1.0.2
  * Author: Ben Wiser
  */
 
@@ -12,11 +12,6 @@ define('NATWEST_PAYIT_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('NATWEST_PAYIT_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 add_action('plugins_loaded', function () {
-
-    if (!class_exists('WC_Payment_Gateway')) {
-        return;
-    }
-
     require_once NATWEST_PAYIT_PLUGIN_PATH . 'includes/class-natwest-payit-loader.php';
     NatWest_PayIt_Loader::init();
 });
